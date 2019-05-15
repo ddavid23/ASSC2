37654c29da4abe1fa45a0455e03148229ef2ce92
ddavid23 <davidstulemeijer@hotmail.nl>
2019-05-15 09:42:27 +0200

    Update ASSC.js

--- ASSC.js
+++ ASSC.js
@@ -162,6 +162,9 @@ client.on("message", (message) => {
   }
 });
 
-
+var http = require("http");
+setInterval(function() {
+    http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
 
 client.login(token);
